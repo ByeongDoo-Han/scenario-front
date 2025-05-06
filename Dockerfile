8dcf9dbb8ffa56d6c7f19605c5197d16d30b78b8
byeongdoohan <quden04@gmail.com>
2025-05-06 21:58:03 +0900

    dockerfile 수정

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ RUN npm run build
 
 FROM nginx:alpine
 
+RUN rm /etc/nginx/conf.d/default.conf
 COPY --from=builder /app/dist /usr/share/nginx/html
 COPY nginx.conf /etc/nginx/nginx.conf
 EXPOSE 80
--- nginx_run.sh
+++ nginx_run.sh
@@ -2,6 +2,4 @@
 
 docker run -d --name scenario-nginx \
   --network scenario-net \
-  -p 80:80 \
-  -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro \
-  -v $(pwd)/dist:/usr/share/nginx/html:ro nginx
+  -p 80:80 nginx:latest
