5445dd9e121956db65108ffc32ec03fb5d24c061
byeongdoohan <quden04@gmail.com>
2025-05-27 20:03:26 +0900

    feat: Home 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Concurrency from "./pages/Concurrency";
 import "./App.css";
 import NplusOne from "./pages/NplusOne";
 import Search from "./pages/Search";
+import Home from "./pages/Home";
 
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<Concurrency />} />
+			<Route path="/" element={<Home />} />
+			<Route path="/concurrency" element={<Concurrency />} />
 			<Route path="/search" element={<Search />} />
 			<Route path="/nplus" element={<NplusOne />} />
 		</Routes>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 	return (
-		<div>
-			<h1>Home</h1>
-			<div></div>
+		<div className="home-container">
+			<h1>시나리오 데모 페이지</h1>
+			<div className="button-container">
+				<Link to="/concurrency" className="nav-button">
+					동시성 제어
+				</Link>
+				<Link to="/nplus" className="nav-button">
+					N+1 문제
+				</Link>
+				<Link to="/search" className="nav-button">
+					ElasticSearch 검색
+				</Link>
+			</div>
 		</div>
 	);
 };
