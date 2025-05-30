3637373fea6fca25275c2b49924c86d5891cc4a5
byeongdoohan <quden04@gmail.com>
2025-05-30 22:55:19 +0900

    home 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,11 @@
 import "./Home.css";
 import About from 	"./About";
-import { useNavigate } from "react-router-dom";
 
 const Home = () => {
 
-	const navigate = useNavigate();
 	return (
 		<About/>
+		
 
 			
 		
