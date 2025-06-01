ec20479dfb357726af2c9b0492a455dbfb318dda
byeongdoohan <quden04@gmail.com>
2025-06-01 22:23:30 +0900

    home nav

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,28 @@
 import "./Home.css";
+import { Nav } from "react-bootstrap";
 
 const Home = () => {
-
+	return (
+		<Nav
+		  activeKey="/home"
+		  onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+		>
+		  <Nav.Item>
+			<Nav.Link href="/home">Active</Nav.Link>
+		  </Nav.Item>
+		  <Nav.Item>
+			<Nav.Link eventKey="link-1">Link</Nav.Link>
+		  </Nav.Item>
+		  <Nav.Item>
+			<Nav.Link eventKey="link-2">Link</Nav.Link>
+		  </Nav.Item>
+		  <Nav.Item>
+			<Nav.Link eventKey="disabled" disabled>
+			  Disabled
+			</Nav.Link>
+		  </Nav.Item>
+		</Nav>
+	  );
 };
 
 export default Home;
